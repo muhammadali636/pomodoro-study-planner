feae10d5a87b3d6db95dc80fd6953ea922543a45
muhammadali636 <muhammadali.toronto@gmail.com>
2025-01-18 12:56:46 -0500

    fixed issue with backend route and refined script

--- backend/index.js
+++ backend/index.js
@@ -1,39 +1,66 @@
-//basic express server with routes (CRUD)
-const express = require('express')
-const mongoose = require('mongoose')
-const cors = require('cors')
-const StudyModel = require('./Models/Study')
+const express = require('express'); 
+const mongoose = require('mongoose');
+const cors = require('cors'); 
+const StudyModel = require('./Models/Study'); 
 
-const app = express()
-app.use(cors())
-app.use(express.json())
+const app = express(); //init the Express app
+app.use(cors()); 
+app.use(express.json()); 
 
-//connect test DB and store data in studdylist collection
+//DB connections
 mongoose.connect('mongodb://localhost:27017/test', {
   useNewUrlParser: true,
-  useUnifiedTopology: true
-})
-app.get('/get', (req, res) => {
+  useUnifiedTopology: true,
+});
+
+//GET ALLLL tasks
+app.get('/get', function(req, res) {
   StudyModel.find()
-    .then(result => res.json(result))
-    .catch(err => res.json(err))
-})
-app.post('/add', (req, res) => {
-  const { task } = req.body
-  StudyModel.create({ task })
-    .then(result => res.json(result))
-    .catch(err => res.json(err))
-})
-app.put('/update/:id', (req, res) => {
-  const { id } = req.params
+    .then(function(result) {
+      res.json(result); //Send all tasks
+    })
+    .catch(function(err) {
+      res.json(err);
+    });
+});
+
+//add new task
+app.post('/add', function(req, res) {
+  const task = req.body.task; //Get task from request
+  StudyModel.create({ task: task })
+    .then(function(result) {
+      res.json(result); 
+    })
+    .catch(function(err) {
+      res.json(err); 
+    });
+});
+
+//marks task as done
+app.put('/update/:id', function(req, res) {
+  const id = req.params.id; //task id
   StudyModel.findByIdAndUpdate(id, { done: true }, { new: true })
-    .then(result => res.json(result))
-    .catch(err => res.json(err))
-})
-app.delete('/delete/:id', (req, res) => {
-  const { id } = req.params
+    .then(function(result) { 
+      res.json(result);
+    })
+    .catch(function(err) {
+      res.json(err); 
+    });
+});
+
+//Delete a task
+app.delete('/delete/:id', function(req, res) {
+  const id = req.params.id; //task id
   StudyModel.findByIdAndDelete(id)
-    .then(() => res.json({ message: 'Deleted' }))
-    .catch(err => res.json(err))
-})
-app.listen(3001, () => console.log('Server running on 3001'))
+    .then(function() {
+      res.json({ message: 'Deleted' }); 
+    })
+    .catch(function(err) {
+      res.json(err); 
+    });
+});
+
+//start server
+app.listen(3001, function() {
+  console.log('Server running...');
+});
